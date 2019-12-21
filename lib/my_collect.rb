8df0fac31a844398a57e9790a4a1b993e5cef4d7
Pam Cairo <pcairo_90@outlook.com>
2019-12-21 04:12:26 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+list = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+
+def my_collect(array)
+  i = 0 
+  name_collection = []
+  while i < array.length
+    name_collection.push yield(array[i])
 
 
+    i += 1 
+  end
+  name_collection
+end
